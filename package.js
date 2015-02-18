5e419e3124705f3fdf1ee49ea37bdfe4ef6a383e
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:26:10 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-search',
+  summary : 'Semantic UI - Search (official): Single component release of search',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Search.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
