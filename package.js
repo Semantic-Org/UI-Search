360a966fbbf360297915da7d3e50ac6f5706fc7f
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:16:14 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,15 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-search',
-  summary : 'Semantic UI - Search (official): Single component release of search',
-  version : '1.9.2',
+  summary : 'Semantic UI - Search: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Search.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'search.css',
+    'search.js'
+  ], 'client');
 });
