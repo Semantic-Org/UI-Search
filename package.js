3bf6f77bd21e7880a91194a1edecdc100ee94c1f
Pusher Robot <jack@semantic-ui.com>
2015-04-16 12:38:40 -0400

    Updated component to version 1.12.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-search',
   summary : 'Semantic UI - Search: Single component release',
-  version : '1.11.7',
+  version : '1.12.0',
   git     : 'git://github.com/Semantic-Org/UI-Search.git',
 });
 
