5005bb590aa3a3cc041194e65e7a77eb1740a027
Pusher Robot <jack@semantic-ui.com>
2015-08-10 14:44:55 -0400

    Updated component to version 2.0.8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * # Semantic UI 2.0.7 - Search
+ * # Semantic UI 2.0.8 - Search
  * http://github.com/semantic-org/semantic-ui/
  *
  *
@@ -1023,7 +1023,7 @@ module.exports = function(parameters) {
   ;
 };
 
-module.exports.settings = {
+_module.exports.settings = {
 
   name           : 'Search',
   namespace      : 'search',
